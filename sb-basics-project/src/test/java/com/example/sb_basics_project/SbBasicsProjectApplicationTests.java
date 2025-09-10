ba669f1fd706d6244b6a5a1a03d5a17dcff30bb7
maksr8 <158086853+maksr8@users.noreply.github.com>
2025-09-10 13:21:49 +0300

    Initial commit

---
+++ sb-basics-project/src/test/java/com/example/sb_basics_project/SbBasicsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_basics_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBasicsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/sb_basics_project/SbBasicsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_basics_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBasicsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBasicsProjectApplication.class, args);
+	}
+
+}
